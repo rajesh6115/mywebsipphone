bb41939e5c006cf0b923f774db450ae9096c05bb
Rajesh Kumar Sahoo <sahoorajesh.d@gmail.com>
2018-04-14 05:50:45 +0530

    [pjsip] basic pjsip config for testing

--- extensions.lua
+++ extensions.lua
@@ -7,9 +7,16 @@ TRUNKMSD = 1
 function default_context(context, extension)
 	app.NoOp("call should not come here "..context.." <"..extension..">")
 	app.Dial('PJSIP/'..extension)
+	app.Hangup()
+end
+function customer_care(context, extension)
+	app.NoOp('Testing One way')
+	app.PlayBack('hello-world')
+	app.Hangup()
 end
 extensions = {
 	['default'] = {
+		['121'] = customer_care,
 		['_X.'] = default_context
 	};
 }
